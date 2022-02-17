e51d5835b67d9d602d64fa9ac3f9371a2c6f6adb
fcraveiro <dextruz@hotmail.com>
2022-02-17 01:28:55 -0300

    Start

--- lib/picker.dart
+++ lib/picker.dart
@@ -1,5 +1,4 @@
 import 'dart:developer';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_image/flutter_native_image.dart';
@@ -27,6 +26,8 @@ var pathServerThumb = '';
 class _PickerState extends State<Picker> {
   SupabaseClient cliente = SupabaseClient(supabaseUrl, supabaseKey);
 
+  XFile? _image;
+
   @override
   void initState() {
     super.initState();
@@ -35,13 +36,85 @@ class _PickerState extends State<Picker> {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Capturar Imagem'),
         centerTitle: true,
         backgroundColor: const Color(0xFF48426D),
       ),
-      body: Center(
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 60,
+          ),
+          GestureDetector(
+            onTap: () {
+              _showSelectImageDialog();
+            },
+            child: SizedBox(
+              height: screenWidth,
+              width: screenWidth,
+//              color: Colors.grey[300],
+              child: _image == null
+                  ? const Center(
+                      child: SizedBox(
+                        width: 160,
+                        height: 160,
+                        child: Icon(
+                          Icons.add_a_photo,
+                          color: Color(0xFF48426D),
+                          size: 150,
+                        ),
+                      ),
+                    )
+                  : Image(
+                      image: FileImage(
+                        File(_image!.path),
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _image == null
+                  ? const SizedBox(
+                      width: 130,
+                      height: 40,
+                    )
+                  : SizedBox(
+                      width: 130,
+                      height: 40,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 7,
+                          fixedSize: const Size(220, 35),
+                          primary: const Color(0xFF48426D),
+                          onSurface: Colors.black,
+                        ),
+                        onPressed: () {
+                          gravacao();
+                          setState(() {
+                            temFoto = false;
+                          });
+                          _showToast(context);
+                        },
+                        child: const Text('Salvar'),
+                      ),
+                    ),
+            ],
+          ),
+        ],
+      ),
+
+      /*
+      Center(
         child: SizedBox(
           width: MediaQuery.of(context).size.width - 10,
           height: MediaQuery.of(context).size.height - 100,
@@ -108,6 +181,7 @@ class _PickerState extends State<Picker> {
           ),
         ),
       ),
+      */
     );
   }
 
@@ -118,30 +192,9 @@ class _PickerState extends State<Picker> {
       pathFoto = File(image.path);
       setState(() {
         temFoto = true;
+        _image = image;
       });
     }
-  }
-
-  gravaFoto(String nomeDaFoto, File pathFoto) async {
-    await cliente.storage
-        .from('pronto')
-        .upload(nomeDaFoto, pathFoto)
-        .then((value) {
-      var response = cliente.storage.from('pronto').getPublicUrl(nomeDaFoto);
-      pathImage = response.data.toString();
-      // ignore: avoid_print
-//      print('Path Servidor Normal : $pathServerNormal');
-    });
-  }
-
-  geraThumb(File foto) async {
-    File response = await FlutterNativeImage.compressImage(
-      foto.path,
-      quality: 65,
-      percentage: 25,
-    );
-    pathFoto = File(response.path);
-    return pathFoto;
   }
 
   gravacao() async {
@@ -158,6 +211,29 @@ class _PickerState extends State<Picker> {
     foiSalva = true;
     log(pathServerThumb);
     log(pathServerNormal);
+    setState(() {
+      _image = null;
+    });
+  }
+
+  gravaFoto(String nomeDaFoto, File pathFoto) async {
+    await cliente.storage
+        .from('pronto')
+        .upload(nomeDaFoto, pathFoto)
+        .then((value) {
+      var response = cliente.storage.from('pronto').getPublicUrl(nomeDaFoto);
+      pathImage = response.data.toString();
+    });
+  }
+
+  geraThumb(File foto) async {
+    File response = await FlutterNativeImage.compressImage(
+      foto.path,
+      quality: 65,
+      percentage: 25,
+    );
+    pathFoto = File(response.path);
+    return pathFoto;
   }
 
   void _showSelectImageDialog() {
